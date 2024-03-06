bbfba94b1c859e32454d419885d7c9cec18490fe
Patrick Almeida <patrickandreia2505@gmail.com>
2024-03-06 01:22:17 -0300

    fix: ensure first available time to book a service is currentTime + 1h

--- app/barbershop/[id]/_helpers/hours.ts
+++ app/barbershop/[id]/_helpers/hours.ts
@@ -1,4 +1,4 @@
-import { setHours, setMinutes, format, addMinutes } from "date-fns";
+import { setHours, setMinutes, format, addMinutes, addHours } from "date-fns";
 
 interface IGenerateDayTimeListProps {
   date: Date;
@@ -8,17 +8,19 @@ interface IGenerateDayTimeListProps {
 }
 
 // Make this function dynamic so that each barbershop can choose its own start and closing times, as well as interval duration.
+
 export function generateDayTimeList(date: Date): string[] {
-  const startTime = setMinutes(setHours(date, 9), 0); // Set start time to 09:00
+  const currentTime = new Date();
+  const startTime = addHours(setMinutes(setHours(currentTime, currentTime.getHours() + 1), 0), 1); // Horário inicial uma hora depois do horário atual
   const endTime = setMinutes(setHours(date, 21), 0); // Set end time to 21:00
   const interval = 45; // interval in minutes
   const timeList: string[] = [];
 
-  let currentTime = startTime;
+  let current = startTime;
 
-  while (currentTime <= endTime) {
-    timeList.push(format(currentTime, "HH:mm"));
-    currentTime = addMinutes(currentTime, interval);
+  while (current <= endTime) {
+    timeList.push(format(current, "HH:mm"));
+    current = addMinutes(current, interval);
   }
 
   return timeList;
