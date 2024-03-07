3b0857f92d520a0d606ca8362146e4337aff0c5b
Patrick Almeida <patrickandreia2505@gmail.com>
2024-03-06 21:57:24 -0300

    fix: footer copyRight year

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="w-full bg-secondary py-6 px-5 mt-auto">
-    <p className="text-gray-400 text-xs font-bold opacity-75">© 2023 Copyright FSW Barber</p>
+    <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright FSW Barber</p>
   </footer>
 );
 
