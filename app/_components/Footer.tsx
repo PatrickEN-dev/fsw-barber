f6e0bafde7ac75527ef22331b2b8e0265ed15807
Patrick Almeida <patrickandreia2505@gmail.com>
2024-03-10 00:12:06 -0300

    feat: add version to project

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,6 +1,7 @@
 const Footer = () => (
-  <footer className="w-full bg-secondary py-6 px-5 mt-auto">
+  <footer className="w-full bg-secondary py-6 px-5 mt-auto flex justify-between">
     <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright FSW Barber</p>
+    <p className="text-gray-400 text-xs font-bold opacity-75">Apha: 0.1.0</p>
   </footer>
 );
 
